42a1ba431a6bf6825ab15b873b96805ce1ea44b0
Furyzxc <serhii.ananiev2@mail.dcu.ie>
2026-03-09 10:02:05 +0000

    moved makefile

--- Stage1/src/help.c
+++ Stage1/src/help.c
@@ -3,7 +3,7 @@
 
 void help() {  // prints manual
   printf("cd <dir>     change the current directory\n");
-  printf("pwd          present working directory\n")
+  printf("pwd          present working directory\n");
   printf("clr          clear the screen\n");
   printf("dir <dir>    list the contents of directory <dir>\n");
   printf("environ      list all environment variables\n");
